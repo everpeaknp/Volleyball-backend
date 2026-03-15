61bd956c481492428be5f277cf6a66ad63495bbd
nirajadh <nirajadhikari12345@gmail.com>
2026-03-15 12:54:45 +0545

    Serve media URLs in production when DEBUG is false

--- volleyball_cms/urls.py
+++ volleyball_cms/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from django.shortcuts import redirect
 
@@ -14,7 +15,9 @@ urlpatterns = [
 # Serve uploaded media files.
 # In this deployment, a dedicated web server is not handling /media,
 # so Django must expose MEDIA_URL even when DEBUG is False.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
